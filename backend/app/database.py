239faaea32bd3999c0e4e8df242403fa05129841
Anas Mohamed <anasmohamed11220@gmail.com>
2026-09-10 16:11:04 +0530

    Improved backend by adding WAL

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,10 +1,12 @@
 import os
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
+from sqlalchemy import create_engine, event
+from sqlalchemy.orm import declarative_base, sessionmaker
+
 
 load_dotenv()
+
 
 DATA_DIR = os.getenv("DATA_DIR")
 
@@ -16,6 +18,7 @@ else:
         "sqlite:///./terralens.db",
     )
 
+
 if DATABASE_URL.startswith("postgresql://"):
     connect_args = {}
 else:
@@ -23,10 +26,23 @@ else:
         "check_same_thread": False,
     }
 
+
 engine = create_engine(
     DATABASE_URL,
     connect_args=connect_args,
 )
+
+
+if DATABASE_URL.startswith("sqlite"):
+    @event.listens_for(engine, "connect")
+    def set_sqlite_pragmas(dbapi_connection, connection_record):
+        cursor = dbapi_connection.cursor()
+
+        cursor.execute("PRAGMA journal_mode=WAL;")
+        cursor.execute("PRAGMA busy_timeout=5000;")
+
+        cursor.close()
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -39,6 +55,7 @@ Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
+
     try:
         yield db
     finally:
